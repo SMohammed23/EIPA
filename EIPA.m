d83c2348d7f137f46d75b2400098cd0d572f1e78
SaifuddinMo <saifumsn@gmail.com>
2021-02-12 14:51:32 -0500

    PA5

---
+++ EIPA.m
@@ -0,0 +1,58 @@
+%%ELEC 4700 PA-5
+%%Saifuddin Mohammed
+%%101092039
+
+set(0,'DefaultFigureWindowStyle','docked')
+set(0,'defaultaxesfontsize',20)
+set(0,'defaultaxesfontname','Times New Roman')
+set(0,'DefaultLineLineWidth',2); 
+close all
+
+
+nx = 50;
+ny = 50;
+%ny = 30;
+G = sparse(nx*ny,nx*ny);
+
+    
+for i = 1:nx
+    for j = 1:ny
+        
+        n = j + (i-1)*ny;
+        
+        if i == 1 || i == nx ||j == 1 || j == ny
+            G(n, :) = 0;
+            G(n, n) = 1;
+            
+        else
+            
+            
+            G(n, n) = -4; % or -2; 
+            G(n, n+1) = 1;
+            G(n, n-1) = 1;
+            G(n, n+ny) = 1;
+            G(n, n-ny) = 1;
+            
+        end
+    end
+end
+
+
+figure(1) 
+spy(G)
+[E, D] = eigs(G, 9, 'SM'); %PLOTTING EIGENVALUES
+
+V = zeros(nx, ny, 9);
+
+
+figure(2)
+
+
+for z = 1:9              
+   V(:, :, z) = reshape(E(:, z), nx, ny);
+   
+   subplot(3, 3, z)
+   
+   surf(V(:, :, z))
+   
+end
